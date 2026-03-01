cdcf0b09bd1b0c1d8e59b5da120af777838f0ce7
Daffa Muhammad Al Fatih <daffamalfatih.3135@gmail.com>
2026-03-01 23:15:07 +0700

    fix: remove next-on-pages from next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
 import type { NextConfig } from "next";
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-
-// Inject Cloudflare bindings (D1) locally for `npm run dev`
-if (process.env.NODE_ENV === "development") {
-  setupDevPlatform();
-}
 
 const nextConfig: NextConfig = {
   /* config options here */
